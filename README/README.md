ea68f35768985e27535b9e3ab115073523d7984f
derek <derekchili@gmail.com>
2023-04-19 16:23:11 -0700

    updated files and added JS and NodeJs

---
+++ README/README.md
@@ -0,0 +1,38 @@
+
+<d>
+
+## Description
+d
+
+## Table of Contents
+d
+- [Installation](#installation)
+- [Usage](#usage)
+- [Credits](#credits)
+- [License](#license)
+
+## Installation
+d
+
+![undefined](../assets/screenshot.png)
+
+## Usage
+d
+
+## Credits
+f
+
+## License
+[https://choosealicense.com/](https://choosealicense.com/).
+MIT
+
+## Badges
+![badmath](https://img.shields.io/github/languages/top/lernantino/badmath)
+MIT
+
+## Contributing
+undefined
+
+##  Questions
+[https://github.com/Derekchili]
+[derekchili@gmail.com]
--- index.js
+++ index.js
@@ -1,8 +1,9 @@
+
 // TODO: Include packages needed for this application
 const fs = require('fs');
 const inquirer = require('inquirer')
 // TODO: Create an array of questions for user input
-const questions = [
+inquirer.prompt([
     {
         type: 'input',
         message: 'What is your name?',
@@ -10,44 +11,110 @@ const questions = [
     },
     {
         type: 'input',
-        message: 'Where are you located?',
-        name: 'location',
+        message: 'What is the title of your project?',
+        name: 'Title',
     },
     {
         type: 'input',
-        message: 'Tell me about yourself?',
-        name: 'bio',
+        message: 'Describe what this app is going to be used for?',
+        name: 'Description',
     },
     {
         type: 'input',
-        message: 'Whats your Linkedin url?',
-        name: 'linkedin',
+        message: 'If you want to add a table of contents do so here?',
+        name: 'Table',
     },
     {
         type: 'input',
-        message: 'Whats your Github url?',
-        name: 'github',
+        message: 'What are the steps required to install your project? Provide a step-by-step description of how to get the development environment running.',
+        name: 'Installation',
+    },
+    {
+        type: 'input',
+        message: 'Please list any usage instructions here?',
+        name: 'Usage',
+    },
+    {
+        type: 'input',
+        message: 'If you have a screenshot and have added it to the assets folder please put the file name here',
+        name: 'Picture',
+    },
+    {
+        type: 'input',
+        message: 'List your collaborators, if any, with links to their GitHub profiles.',
+        name: 'Credits',
+    },
+    {
+        type: 'checkbox',
+        message: 'Did you add a License to this project, please tell us which one.',
+        name: 'License',
+        choices: ['MIT', 'Apache', 'BSD']
+    }, 
+    {
+        type: 'checkbox',
+        message: 'Did you add a Badge to this project, please tell us which one.',
+        name: 'Badge',
+        choices: ['MIT', 'Apache', 'BSD']
+    }, 
+    {
+        type: 'input',
+        message: 'Whats your GitHub username?',
+        name: 'Github'
     },
     {
         type: 'input',
         message: 'Whats your Email?',
-        name: 'email',
+        name: 'Email'
     },
-   
-];
+    
+]).then(answers => {
+    let readme = `
+<${answers.Title}>
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {
-    fs.writeFile('result.json',JSON.stringify(fileName,null,4(err))
-    error ? console.log(error): console.log(data));
-}
+## Description
+${answers.Description}
+
+## Table of Contents
+${answers.Table}
+- [Installation](#installation)
+- [Usage](#usage)
+- [Credits](#credits)
+- [License](#license)
+
+## Installation
+${answers.Installation}
+
+![${answers.alt}](../assets/${answers.Picture})
+
+## Usage
+${answers.Usage}
+
+## Credits
+${answers.Credits}
+
+## License
+[https://choosealicense.com/](https://choosealicense.com/).
+${answers.License}
+
+## Badges
+![badmath](https://img.shields.io/github/languages/top/lernantino/badmath)
+${answers.Badge}
+
+## Contributing
+${answers.Contributing}
+
+##  Questions
+[https://github.com/${answers.Github}]
+[${answers.Email}]
+`
+fs.writeFile('./README/README.md',readme,(err)=>{
+    if(err){
+        throw err
+    }
+    console.log("Great Job!!");
+})
+});
 
 
-
-// TODO: Create a function to initialize app
-function init() {}
-
-[Inquirer.package]('https://www.npmjs.com/package/inquirer/v/8.2.4');
-
-// Function call to initialize app
-init();
+// license style: ?style=flat&logo=appveyor 
+// badges : https://img.shields.io/static/v1?label=<LABEL>&message=<MESSAGE>&color=<COLOR> color brightgreen
